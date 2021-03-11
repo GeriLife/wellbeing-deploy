a11f9cbb7a80ee2cc19658ba2cd4dafd9d720272
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2021-03-11 14:53:16 +0200

    Reorganize deployment code

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/GeriLife/wellbeing.git
-
-git clone https://github.com/GeriLife/wellbeing-client.git
-
-./node_modules/.bin/concurrently "./deploy-backend.sh" "./deploy-frontend.sh"

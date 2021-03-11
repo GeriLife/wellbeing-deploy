43770ddf22fd5b9f4885628d896e0658cce45b97
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2021-03-11 14:27:35 +0200

    Concurrent deploy front/backend

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/GeriLife/wellbeing.git
 
 git clone https://github.com/GeriLife/wellbeing-client.git
+
+./node_modules/.bin/concurrently "./deploy-backend.sh" "./deploy-frontend.sh"

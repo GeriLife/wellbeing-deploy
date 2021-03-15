27ec74fdad05f08ee28a69e87c3a328a8f330337
Shailee Mehta <>
2021-03-15 22:42:17 +0530

    removing bash resolving command

--- deploy-all.sh
+++ deploy-all.sh
@@ -1,5 +1,4 @@
 cd wellbeing/app && meteor npm i && cd ../../
 cd wellbeing-client/frontend && yarn && cd ../../
 
-echo "export PATH=$PATH:$(npm get prefix)/bin" >> ~/.bashrc
 sh ./deploy-backend.sh
